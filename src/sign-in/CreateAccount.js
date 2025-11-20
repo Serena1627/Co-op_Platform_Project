910f535725e61010d4fc7e5368b70678bf6f0e91
nardos34 <129783939+nardos34@users.noreply.github.com>
2025-11-20 17:15:34 -0500

    trying to fix the supabase built in sign in table to the html

--- src/sign-in/CreateAccount.js
+++ src/sign-in/CreateAccount.js
@@ -1,2 +1,32 @@
-import { supabaseClient } from "../supabaseClient.js";
+import { supabaseClient } from "../../supabaseClient.js";
+let form = document.getElementById('sign-up-form');
 
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    let firstName = document.getElementById('firstName').value;
+    let lastName = document.getElementById('lastName').value;
+    let emailAddress = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let accountType = document.querySelector('input[name="accountType"]:checked').value;
+
+    try {
+        let { data, error } = await supabase.auth.signUp({
+            email: emailAddress,
+            password: password,
+            options: {
+                data: { firstName, lastName, accountType }
+            }
+        });
+        if (error) {
+            alert("Signup failed: " + error.message);
+            return;
+        } 
+
+        form.reset();
+        alert("Account created");
+    } catch (error) {
+        alert("Error");
+    }
+    
+
+});
