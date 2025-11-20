8be1283b5193089c15959cf75300c4da1b67dc0a
Serena Osuagwu <so546@drexel.edu>
2025-11-20 21:40:27 +0000

    testing supabase connection
    (cherry picked from commit b973f21ec651d0bef781ced73fd67060dc42ed8a)
    
    Co-authored-by: Serena Osuagwu <serenityosuagwu@gmail.com>

--- src/studentPages/JobPostings.js
+++ src/studentPages/JobPostings.js
@@ -1,7 +1,12 @@
-import { createClient } from "@supabase/supabase-js";
-let apiKey = process.env.SUPABASE_API_KEY;
-let projectUrl = process.env.SUPABASE_PROJECT_URL;
-export const supabase = createClient(
+
+const supabase = window.supabase.createClient(
     "https://xvdbeuqgtyonbbsdkcqu.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2ZGJldXFndHlvbmJic2RrY3F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwNjUwNjYsImV4cCI6MjA3ODY0MTA2Nn0.ZF-zhsVH2OCyhljoC_G3Rlug5IwZS_OTcdkYwfL1d84"
-)
+);
+
+async function test() {
+    const { data, error } = await supabase.from("job_listings").select("*");
+    console.log(data, error);
+}
+
+test();
